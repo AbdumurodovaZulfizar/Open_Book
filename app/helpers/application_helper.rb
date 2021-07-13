01930c74ccd6ea6b1c43c0b1b70ede94d6005343
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-07-13 16:01:31 +0500

    Add alert and notice

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def alert_btn
+    return render partial: 'layouts/alerts' if flash[:alert]
+  end
+
+  def notice_btn
+    return render partial: 'layouts/notice' if flash[:notice]
+  end
 end
