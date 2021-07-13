0ca25bde5c04bd2fc4366a248aad97a7eb435683
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-07-13 15:34:51 +0500

    Add user model with migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_13_103215) do
+ActiveRecord::Schema.define(version: 2021_07_13_103341) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(version: 2021_07_13_103215) do
   create_table "opinions", force: :cascade do |t|
     t.integer "author_id"
     t.text "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "full_name"
+    t.text "photo"
+    t.text "cover_image"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
