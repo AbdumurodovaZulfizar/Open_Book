b71604fced3ceca8debe69c1b6e2cd9915252cce
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-07-13 15:36:33 +0500

    Generate Vote model with migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_13_103341) do
+ActiveRecord::Schema.define(version: 2021_07_13_103512) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,13 @@ ActiveRecord::Schema.define(version: 2021_07_13_103341) do
     t.string "full_name"
     t.text "photo"
     t.text "cover_image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "votes", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "opinion_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
