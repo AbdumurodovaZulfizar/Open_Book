de5e59c0ffc91c74f1b575d0a543b033eaf519ad
AbdumurodovaZulfizar <abdumurodovazulfizar77@gamil,com>
2021-07-17 19:14:52 +0500

    Remove current user from who-to-follow section

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -74,9 +74,11 @@ module ApplicationHelper
   end
 
   def right_home
-    return unless Current.user
+
+    
 
     User.order(created_at: :desc).all.map do |user|
+      if Current.user != user
       content_tag :div, class: 'right_home my-3' do
         content_tag :div, class: 'row g-0' do
           concat(content_tag(:div, class: 'col-md-3') do
@@ -96,6 +98,7 @@ module ApplicationHelper
           end)
         end
       end
+    end
     end.join.html_safe
   end
 
